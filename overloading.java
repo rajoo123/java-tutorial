548943a1e06c8e22656493729bf394591ffb1d47
rajoo123 <rajooaptec@gmail.com>
2025-11-09 11:46:43 -0800

    add overloading

---
+++ overloading.java
@@ -0,0 +1,44 @@
+class Central_Bank{
+
+    double interate(){
+        return 7.7;
+    }
+}
+
+
+class UBL extends Central_Bank{
+@Override
+
+double interate(){
+    return 10.7;
+}
+ 
+}
+
+class HBL extends Central_Bank{
+    @Override
+    double interate(){
+        return 10.8;
+
+    }
+}
+
+
+
+class overloading{
+    public static void main(String[] args) {
+// System.out.println("Sant Rampal ji Maharaj ji blessing me ");
+
+Central_Bank ub;
+
+ub = new UBL();
+System.out.println("UBL intrest_rate "+ ub.interate());
+ub = new HBL();
+
+
+
+System.out.println("hbl intrest_rate "+ ub.interate());
+
+
+    }
+}
